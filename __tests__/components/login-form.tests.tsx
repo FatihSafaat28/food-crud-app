f7ad7aa5bd080f821edeff424477a6538371c72c
Fatih <fatihsafaat@gmail.com>
2026-02-01 11:56:44 +0700

    add test render login-form

--- __tests__/components/login-form.tests.tsx
+++ __tests__/components/login-form.tests.tsx
@@ -15,8 +15,8 @@ jest.mock('next/navigation', () => ({
 describe("LoginForm - Render fields and inputs", () => {
   const mockingHandleLogin = jest.fn();
 
-  describe("Login mode (isLogin='Sign up')", () => {
-    it("should render email field label and input", () => {
+  describe(`Login mode`, () => {
+    it(`should render email field label and input`, () => {
       render(<LoginForm isLogin={"Sign up"} handleLogin={mockingHandleLogin} />);
 
       // Check for email label
@@ -28,6 +28,63 @@ describe("LoginForm - Render fields and inputs", () => {
       expect(emailInput).toBeInTheDocument();
       expect(emailInput).toHaveAttribute("type", "email");
       expect(emailInput).toHaveAttribute("id", "email");
+      
+    });
+
+    it(`should render password field label and input`, () => {
+      render(<LoginForm isLogin={`Sign up`} handleLogin={mockingHandleLogin} />);
+
+      // Check for password label
+      const passwordLabel = screen.getByText("Password");
+      expect(passwordLabel).toBeInTheDocument();
+
+      // Check for password input
+      const passwordInput = screen.getByPlaceholderText("your password");
+      expect(passwordInput).toBeInTheDocument();
+      expect(passwordInput).toHaveAttribute("type", "password");
+      expect(passwordInput).toHaveAttribute("id", "password");
+    });
+  });
+
+  describe(`Register mode`, () => {
+    it(`should render name field label and input`, () => {
+      render(<LoginForm isLogin={`Sign in`} handleLogin={mockingHandleLogin} />);
+
+      // Check for name label
+      const nameLabel = screen.getByText("Nama");
+      expect(nameLabel).toBeInTheDocument();
+
+      // Check for name input
+      const nameInput = screen.getByPlaceholderText("your name");
+      expect(nameInput).toBeInTheDocument();
+      expect(nameInput).toHaveAttribute("type", "text");
+      expect(nameInput).toHaveAttribute("id", "name");
+    });
+    it(`should render email field label and input`, () => {
+      render(<LoginForm isLogin={`Sign in`} handleLogin={mockingHandleLogin} />);
+
+      // Check for email label
+      const emailLabel = screen.getByText("Email");
+      expect(emailLabel).toBeInTheDocument();
+
+      // Check for email input
+      const emailInput = screen.getByPlaceholderText("example@mail.com");
+      expect(emailInput).toBeInTheDocument();
+      expect(emailInput).toHaveAttribute("type", "email");
+      expect(emailInput).toHaveAttribute("id", "email");
+    });
+    it(`should render password field label and input`, () => {
+      render(<LoginForm isLogin={`Sign in`} handleLogin={mockingHandleLogin} />);
+
+      // Check for password label
+      const passwordLabel = screen.getByText("Password");
+      expect(passwordLabel).toBeInTheDocument();
+
+      // Check for password input
+      const passwordInput = screen.getByPlaceholderText("your password");
+      expect(passwordInput).toBeInTheDocument();
+      expect(passwordInput).toHaveAttribute("type", "password");
+      expect(passwordInput).toHaveAttribute("id", "password");
     });
   });
 
